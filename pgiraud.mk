d9abcf9a955f820123ad51d20d04cec612fef658
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-04-01 12:23:02 +0200

    Update config

--- pgiraud.mk
+++ pgiraud.mk
@@ -1,6 +1,8 @@
 INSTANCE_ID = pgiraud
 DEVELOPMENT = TRUE
 PRINT_OUTPUT = /tmp
-DISABLE_BUILD_RULES = apache
+DISABLE_BUILD_RULES = apache test-packages print
+TILECLOUD_CHAIN = FALSE
+MOBILE = FALSE
 
 include c2cgp_minimal2.mk
